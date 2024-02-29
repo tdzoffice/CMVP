93795ebaeaa80f832e546face69d6795f04f94da
thawdezin <thawdezin@gmail.com>
2024-02-29 08:14:29 +0630

    Initial Commit

---
+++ CMVP/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  CMVP
+//
+//  Created by MAC on 29/02/2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CMVP/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  CMVP
+//
+//  Created by MAC on 29/02/2024.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
